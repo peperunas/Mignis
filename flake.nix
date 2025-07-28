2ba93edebeaf006e5f506da27ff7e112725a55f9
Giulio De Pasquale <git@depasquale.giugl.io>
2025-07-28 16:34:10 +0100

    plz

--- flake.nix
+++ flake.nix
@@ -11,31 +11,12 @@
       (system:
         let
           pkgs = import nixpkgs { inherit system; };
+
+          mignisPkg = pkgs.callPackage ./package.nix;
         in
         {
-
           packages = rec {
-            mignis = pkgs.python3Packages.buildPythonApplication {
-              pname = "mignis";
-              version = "0.9.5";
-
-              src = ./.;
-
-              propagatedBuildInputs = [ pkgs.python3Packages.ipaddr ];
-
-              installPhase = ''
-                mkdir -p $out/bin
-                cp mignis.py $out/bin/mignis
-                chmod +x $out/bin/mignis
-              '';
-
-              meta = with pkgs.lib; {
-                description = "A semantic based tool for firewall configuration";
-                homepage = "https://github.com/secgroup/Mignis";
-                license = licenses.mit;
-              };
-            };
-
+            mignis = mignisPkg;
             default = mignis;
           };
         })

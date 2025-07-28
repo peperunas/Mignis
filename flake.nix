dc1bae8294e822c82b81957c9d2ae78f526af625
Giulio De Pasquale <git@depasquale.giugl.io>
2025-07-28 11:35:53 +0100

    feat(flake): add Nix flake configuration

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A Nix flake for Mignis firewall";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+
+        packages = rec {
+          mignis = pkgs.python3Packages.buildPythonApplication {
+            pname = "mignis";
+            version = "0.9.5";
+
+            src = ./.;
+
+            propagatedBuildInputs = [ pkgs.python3Packages.ipaddr ];
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp mignis.py $out/bin/mignis
+              chmod +x $out/bin/mignis
+            '';
+
+            meta = with pkgs.lib; {
+              description = "A semantic based tool for firewall configuration";
+              homepage = "https://github.com/secgroup/Mignis";
+              license = licenses.mit;
+            };
+          };
+
+          nixosModules.mignis = import ./mignis.nix {
+            mignis-pkg = mignis;
+          };
+
+          default = mignis;
+        };
+      });
+}
---
+++ mignis.nix
@@ -0,0 +1,112 @@
+{ config, pkgs, mignis-pkg }:
+
+with pkgs.lib;
+
+let
+  cfg = config.services.mignis;
+
+  mignisConf = pkgs.writeText "mignis.conf" ''
+    OPTIONS
+    ${optionalString (cfg.options.default_rules) "default_rules yes"}
+    ${optionalString (cfg.options.logging) "logging yes"}
+
+    INTERFACES
+    ${concatStringsSep "\n" (mapAttrsToList (name: value: "${name} ${value.interface} ${value.subnet} ${optionalString (value.ignore) "ignore"}") cfg.interfaces)}
+
+    ALIASES
+    ${concatStringsSep "\n" (mapAttrsToList (name: value: "${name} ${value}") cfg.aliases)}
+
+    FIREWALL
+    ${concatStringsSep "\n" cfg.firewall}
+
+    POLICIES
+    ${concatStringsSep "\n" cfg.policies}
+
+    CUSTOM
+    ${concatStringsSep "\n" cfg.custom}
+  '';
+in
+{
+  options.services.mignis = {
+    enable = mkEnableOption "Mignis firewall";
+
+    options = {
+      default_rules = mkOption {
+        type = types.bool;
+        default = true;
+        description = "Enable Mignis default rules.";
+      };
+      logging = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Enable logging of dropped packets.";
+      };
+    };
+
+    interfaces = mkOption {
+      type = types.attrsOf (types.submodule {
+        options = {
+          interface = mkOption {
+            type = types.str;
+            description = "Name of the network interface (e.g., eth0).";
+          };
+          subnet = mkOption {
+            type = types.str;
+            description = "Subnet in CIDR notation (e.g., 192.168.1.0/24).";
+          };
+          ignore = mkOption {
+            type = types.bool;
+            default = false;
+            description = "Ignore traffic on this interface.";
+          };
+        };
+      });
+      default = {};
+      description = "Network interfaces for Mignis.";
+    };
+
+    aliases = mkOption {
+      type = types.attrsOf types.str;
+      default = {};
+      description = "IP address aliases.";
+    };
+
+    firewall = mkOption {
+      type = types.listOf types.str;
+      default = [];
+      description = "Firewall rules in Mignis syntax.";
+    };
+
+    policies = mkOption {
+      type = types.listOf types.str;
+      default = [];
+      description = "Default policies for unmatched packets.";
+    };
+
+    custom = mkOption {
+      type = types.listOf types.str;
+      default = [];
+      description = "Custom raw iptables rules.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ mignis-pkg ];
+
+    systemd.services.mignis-firewall = {
+      description = "Mignis Firewall";
+      wantedBy = [ "multi-user.target" ];
+      before = [ "network.target" ];
+
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+
+      script = ''
+        echo "Applying Mignis firewall rules..."
+        ${mignis-pkg}/bin/mignis -c ${mignisConf} -e --force
+      '';
+    };
+  };
+}
